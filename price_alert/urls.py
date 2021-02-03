7c71fdc4851c09e603b569ea47c840a02821ebac
simna <simna@labglo.com>
2021-02-03 18:03:24 +0530

    Copied from older version

--- price_alert/urls.py
+++ price_alert/urls.py
@@ -5,4 +5,5 @@ from price_alert import views
 urlpatterns = [
     path('create/', views.alert_create),
     path('list/', views.alert_list),
+    path('mark_as_inactive/', views.mark_as_inactive),
 ]
--- price_alert/views.py
+++ price_alert/views.py
@@ -1,5 +1,9 @@
+import datetime, requests, urllib
+from django.conf import settings
+from django.core.mail import EmailMessage
 from django.http import JsonResponse
 from django.forms import ModelForm
+from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
 
 from price_alert.models import Alert
@@ -73,3 +77,96 @@ def alert_list(request):
         row["current_offer_price"] = each.current_offer_price
         data.append(row)
     return JsonResponse({'alerts': data}, status=200)
+
+
+def get_email_content(instance):
+    try:
+        api_url = "https://matchos.price.com/apiv2/match_recommend?partner=5b960a4ade2962062eeb1f4a&url={}".format(
+            urllib.quote(instance.product_url)
+        )
+        data = requests.get(api_url).json()[0]
+        print api_url
+    except Exception as e:
+        data = {}
+    count_stats = {}
+    for each in data.get("matches", []):
+        if each["skuCondition"]+"_count" not in count_stats.keys():
+            count_stats[each["skuCondition"]+"_count"] = 1
+            count_stats[each["skuCondition"]+"_lowest_price"] = each["salePrice"]
+        else:
+            count_stats[each["skuCondition"]+"_count"] += 1
+            if each["salePrice"] < count_stats[each["skuCondition"]+"_lowest_price"]:
+                count_stats[each["skuCondition"]+"_lowest_price"] = each["salePrice"]
+    result = {}
+    result['Count'] = count_stats
+    result["current_price"] = data.get("recommendation", {}).get("salePrice")
+
+    result["product"] = data.get("recommendation")
+    result["alert"] = instance
+    try:
+        result["you_save"] = instance.current_offer_price - instance.notified_price
+    except:
+        result["you_save"] = None
+
+    print ("result", result)
+    # get title, image_url
+    try:
+        api_url = "https://matchos.price.com/apiv2/item?partner=5b960a4ade2962062eeb1f4a&url={}".format(urllib.quote(instance.product_url))
+        data = requests.get(api_url).json()[0]
+    except Exception as e:
+        data = []
+    if data:
+        result["title"] = data.get("productName")
+        result["image_url"] = data.get("imageUrl")
+    alt_title = result["product"].get("productName")
+    alt_image_url = result["product"].get("imageUrl")
+    if not result.get("title"):
+        result["title"] = alt_title
+    if not result.get("image_url"):
+        result["image_url"] = alt_image_url
+    return result
+
+
+def mark_as_inactive(request):
+    status_code = 400
+    updated = False
+    message = None
+    uuid = request.GET.get('uuid', '').strip()
+    notified_price = request.GET.get('notified_price', '').strip()
+    try:
+        instance = Alert.objects.get(pk=uuid)
+    except Exception as e:
+        instance = None
+        message = str(e)
+    try:
+        notified_price = float(notified_price)
+    except Exception as e:
+        message = "Invalid Price"
+    if not notified_price:
+        return JsonResponse({'updated':updated, 'message':"notified_price missing"}, status=400)
+    if not message and instance:
+        if instance.active:
+            instance.active=False
+            instance.notified_price = notified_price
+            instance.notified_timestamp = datetime.datetime.now()
+            result = get_email_content(instance)
+            instance.save()
+            message = "Marked as Inactive"
+            updated = True
+            status_code = 200
+            subject = "Price Drop on {}".format(result["title"][:40])
+            content = render_to_string("webapp/PriceAlert/email.html", {'result': result})
+            from_email = settings.DEFAULT_FROM_EMAIL
+            # html_content = htmly.render(data)
+            to_email = [instance.user.email]
+            #to_email = ["team@price.com", "test@price.com", "rj@price.com", "jayapal@price.com", "vasco@price.com"]
+            msg = EmailMessage(subject, content, from_email, to_email)
+            msg.content_subtype = "html"  # Main content is now text/html
+            if instance.notified_price:
+                msg.send()
+
+            #return render(request, 'webapp/PriceAlert/email.html', {'result': result})
+
+        else:
+            message = "Already marked as Inactive"
+    return JsonResponse({'updated':updated, 'message':message}, status=200)
